93303d47d25024bb7f1e3576db33d20294a0ab01
Matt Withem <mw767219@ohio.edu>
2020-01-22 19:20:19 -0500

    Practice Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Matt Withem>
+ *        Date: <1/22/2020>
+ * Description: Program for lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
